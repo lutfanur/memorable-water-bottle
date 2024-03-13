7523a8a17bd90a133037a168655c2804b05bdd38
Lutfaakter <lutfanur20@gmail.com>
2024-03-13 09:44:34 +0000

    water bottle remove from local storage

--- src/components/Bottles/Bottles.jsx
+++ src/components/Bottles/Bottles.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useState } from "react";
 import Bottle from "../Bottle/Bottle";
 import './Bottles.css'
-import { addToLS, getStoredCart } from "../../utilities/localstorage";
+import { addToLS, getStoredCart, removeFromLS } from "../../utilities/localstorage";
 import Cart from "../Cart/Cart";
 
 const Bottles = () => {
@@ -20,13 +20,13 @@ const Bottles = () => {
         console.log('called the useEffect', bottles.length)
         if (bottles.length > 0) {
             const storedCart = getStoredCart();
-            console.log(storedCart,bottles);
+            console.log(storedCart, bottles);
             const savedCart = [];
 
-            for(const id of storedCart){
+            for (const id of storedCart) {
                 console.log(id);
                 const bottle = bottles.find(bottle => bottle.id === id);
-                if(bottle){
+                if (bottle) {
                     savedCart.push(bottle);
                 }
             }
@@ -44,11 +44,19 @@ const Bottles = () => {
         addToLS(bottle.id)
     }
 
+    const handleRemoveFromCart = id => {
+        // visual cart remove
+        const remainingCart = cart.filter(bottle =>bottle.id !== id);
+        setCart(remainingCart);
+        // remove from local storage
+        removeFromLS(id)
+    }
+
 
     return (
         <div>
             <h2>Bottles Available: {bottles.length}</h2>
-            <Cart cart={cart}></Cart>
+            <Cart cart={cart} handleRemoveFromCart={handleRemoveFromCart}></Cart>
             <div className="bottle-container">
                 {
                     bottles.map(bottle => <Bottle
